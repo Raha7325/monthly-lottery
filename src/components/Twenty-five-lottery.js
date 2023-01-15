3078a297b1c2c92c4efc9657047ae1b0f17aa966
r.kashir <r.kashir@chabok.io>
2023-01-15 18:49:04 +0330

    feat: lottery for 1401-10-25

--- src/components/Twenty-five-lottery.js
+++ src/components/Twenty-five-lottery.js
@@ -9,8 +9,8 @@ class TwentyFiveLottery extends Component {
           <div className='mb-10 px-3 py-1 rounded-md bg-amber-200 text-center text-amber-500 font-bold'>صندوق ۲۵ ام <span className='text-xs'>(شروع از ۲۵-۰۸-۱۴۰۱)</span></div>
           <div className='grid grid-cols-3 gap-x-4 gap-y-5 place-items-center'>
           <div>
-           <div className='w-[50px] h-[50px] rounded-[50px] shadow-md shadow-amber-500 text-amber-500 bg-amber-100 flex items-center justify-center col-span-1'>۳</div>
-           <h5 className='text-xs text-white pt-2'>.</h5>
+           <div className='w-[50px] h-[50px] rounded-[50px] shadow-md shadow-gray-200 text-gray-100 bg-gray-200 flex items-center justify-center col-span-1'>۳</div>
+           <h5 className='text-xs text-white pt-2'>۱۴۰۱-۱۰-۲۵</h5>
           </div>
           <div>
             <div className='w-[50px] h-[50px] rounded-[50px] shadow-md shadow-gray-200 text-gray-100 bg-gray-200 flex items-center justify-center col-span-1'>۲</div>
