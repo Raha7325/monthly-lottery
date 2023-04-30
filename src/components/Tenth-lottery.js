4ae256f909a6019a396d527dc41a66dac167afda
r.kashir <r.kashir@chabok.io>
2023-04-30 14:01:51 +0330

    enhnace: update tenth lottery

--- src/components/Tenth-lottery.js
+++ src/components/Tenth-lottery.js
@@ -44,7 +44,10 @@ class TenthLottery extends Component {
            <div className='w-[50px] h-[50px] rounded-[50px] shadow-md shadow-rose-400 text-rose-400 bg-rose-100  flex items-center justify-center col-span-1'>۷</div>
            <h5 className='text-xs text-transparent pt-2'> .</h5>
           </div> 
-           <div className='w-[50px] h-[50px] rounded-[50px]	shadow-md shadow-rose-400 text-rose-400 bg-rose-100 flex items-center justify-center col-span-3'>۱۰</div>
+          <div className='col-span-3'>
+          <div className='w-[50px] h-[50px] rounded-[50px]	shadow-md shadow-gray-200 text-gray-100 bg-gray-200 flex items-center justify-center col-span-3'>۱۰</div>
+            <h5 className='text-xs text-gray-200 pt-2'>۱۴۰۲-۰۲-۱۰</h5>
+          </div>
           </div>
         </div>
       </>
