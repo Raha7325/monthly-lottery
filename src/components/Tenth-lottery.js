e017f6c54554f481628182ecf9a697e82bf69f32
r.kashir <r.kashir@chabok.io>
2023-07-01 13:21:03 +0330

    set currect position

--- src/components/Tenth-lottery.js
+++ src/components/Tenth-lottery.js
@@ -16,7 +16,7 @@ class TenthLottery extends Component {
            <div className='w-[50px] h-[50px] rounded-[50px] shadow-md shadow-gray-200 text-gray-100 bg-gray-200 flex items-center justify-center col-span-1'>۲</div>
            <h5 className='text-xs text-gray-200 pt-2'>۱۴۰۱-۱۰-۱۰</h5>
           </div>          
-          <div className='flex self-start'>
+          <div>
            <div className='w-[50px] h-[50px] rounded-[50px] shadow-md shadow-gray-200 text-gray-100 bg-gray-200 flex items-center justify-center col-span-1'>۱</div>
            <h5 className='text-xs text-gray-200 pt-2'>۱۴۰۲-۰۴-۱۰</h5>
           </div>             
