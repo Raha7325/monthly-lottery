c9ae5e92d8987584f8d66ca456a7ef6c6e4eed61
r.kashir <r.kashir@chabok.io>
2023-07-16 19:18:59 +0330

    updating twenty-five lottery

--- src/components/Twenty-five-lottery.js
+++ src/components/Twenty-five-lottery.js
@@ -29,8 +29,8 @@ class TwentyFiveLottery extends Component {
             <h5 className='text-xs text-gray-200 pt-2'>۱۴۰۲-۰۱-۲۵</h5>
           </div>
           <div>
-           <div className='w-[50px] h-[50px] rounded-[50px] shadow-md shadow-amber-500 text-amber-500 bg-amber-100 flex items-center justify-center col-span-1'>۴</div>
-           <h5 className='text-xs text-transparent pt-2'>.</h5>
+           <div className='w-[50px] h-[50px] rounded-[50px] shadow-md shadow-gray-200 text-gray-100 bg-gray-200 flex items-center justify-center col-span-1'>۴</div>
+           <h5 className='text-xs text-gray-200 pt-2'>۱۴۰۲-۰۴-۲۵</h5>
           </div>
           <div>
            <div className='w-[50px] h-[50px] rounded-[50px] shadow-md shadow-gray-200 text-gray-100 bg-gray-200 flex items-center justify-center col-span-1'>۹</div>
