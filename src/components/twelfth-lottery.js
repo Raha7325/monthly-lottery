d4820a1a270ea8fab1b94949a179fac8766d8184
Faezeh keshir <86400725+Raha7325@users.noreply.github.com>
2023-08-03 15:09:18 +0330

    Update twelfth-lottery.js

--- src/components/twelfth-lottery.js
+++ src/components/twelfth-lottery.js
@@ -21,8 +21,8 @@ class FifteenthLottery extends Component {
            <h5 className='text-xs text-gray-200 pt-2'>۱۴۰۲-۰۴-۱۲</h5>
           </div>          
           <div>
-            <div className='w-[50px] h-[50px] rounded-[50px] shadow-md shadow-indigo-400 text-indigo-400 bg-indigo-100 flex items-center justify-center col-span-1'>۶</div>
-            <h5 className='text-xs text-transparent pt-2'> .</h5>
+            <div className='w-[50px] h-[50px] rounded-[50px] shadow-md shadow-gray-200 text-gray-100 bg-gray-200 flex items-center justify-center col-span-1'>۶</div>
+           <h5 className='text-xs text-gray-200 pt-2'>۱۴۰۲-۰۵-۱۲</h5>
           </div>          
           <div>
            <div className='w-[50px] h-[50px] rounded-[50px] shadow-md shadow-indigo-400 text-indigo-400 bg-indigo-100 flex items-center justify-center col-span-1'>۵</div>
