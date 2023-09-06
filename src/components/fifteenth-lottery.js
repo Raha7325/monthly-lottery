832019ff84132360b1c8798e0e9797ae3b2ed68e
raha <raha@pop-os.localdomain>
2023-09-06 14:51:50 +0330

    updated fifteenth lottery

--- src/components/fifteenth-lottery.js
+++ src/components/fifteenth-lottery.js
@@ -44,7 +44,10 @@ class FifteenthLottery extends Component {
            <div className='w-[50px] h-[50px] rounded-[50px] shadow-md shadow-cyan-400 text-cyan-400 bg-cyan-100 flex items-center justify-center col-span-1'>۷</div>
            <h5 className='text-xs text-transparent pt-2'> .</h5>
           </div>
-           <div className='w-[50px] h-[50px] rounded-[50px]	shadow-md shadow-cyan-400 text-cyan-400 bg-cyan-100 flex items-center justify-center col-span-3'>۱۰</div>
+          <div className='col-span-3'>
+          <div className='w-[50px] h-[50px] rounded-[50px] shadow-md shadow-gray-200 text-gray-100 bg-gray-200 flex items-center justify-center'>۱۰</div>
+           <h5 className='text-xs text-gray-200 pt-2 flex justify-center'>۱۴۰۲-۰۶-۱۵</h5>
+          </div>
           </div>
         </div>
       </>
