3244d14593ae5543484b15d233fe166670ebf560
raha <raha@pop-os.localdomain>
2023-11-15 13:42:56 +0330

    enhance: updated twenty-four lottery

--- src/components/twenty-four-lottery.js
+++ src/components/twenty-four-lottery.js
@@ -37,8 +37,8 @@ class TwentyFourLottery extends Component {
            <h5 className='text-xs text-transparent pt-2'> .</h5>
           </div>             
           <div>
-           <div className='w-[50px] h-[50px] rounded-[50px] shadow-md shadow-fuchsia-400 text-fuchsia-400 bg-fuchsia-100 flex items-center justify-center col-span-1'>۸</div>
-           <h5 className='text-xs text-transparent pt-2'> .</h5>
+           <div className='w-[50px] h-[50px] rounded-[50px] shadow-md shadow-md shadow-gray-200 text-gray-100 bg-gray-200 flex items-center justify-center col-span-1'>۸</div>
+           <h5 className='text-xs text-gray-200 pt-2'>۱۴۰۲-۰۸-۲۴</h5>
           </div>  
           <div>
            <div className='w-[50px] h-[50px] rounded-[50px] shadow-md shadow-fuchsia-400 text-fuchsia-400 bg-fuchsia-100 flex items-center justify-center col-span-1'>۷</div>
