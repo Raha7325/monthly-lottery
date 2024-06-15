278f515eed87509e1379aecfd668881b6637b6b4
r.kashir <workkashir2323@gmail.com>
2024-06-15 17:21:13 +0330

    updated 20th and 24th

--- src/components/twenty-four-lottery.js
+++ src/components/twenty-four-lottery.js
@@ -6,16 +6,16 @@ class TwentyFourLottery extends Component {
     return(
       <>
         <div className='flex-col mt-[40px] md:mt-0 shadow-md p-4'>
-          <div className='mb-10 px-3 py-1 rounded-md bg-fuchsia-100 text-center text-fuchsia-400 font-bold'>صندوق ۲۴ ام <span className='text-xs'>(شروع از ۲۴-۰۶-۱۴۰۲)</span></div>
+          <div className='mb-10 px-3 py-1 rounded-md bg-gray-200 text-center text-gray-100 font-bold'>صندوق ۲۴ ام <span className='text-xs'>(شروع از ۲۴-۰۶-۱۴۰۲)</span></div>
           <div className='grid grid-cols-3 gap-x-4 gap-y-5 place-items-center'>
           <div>
            <div className='w-[50px] h-[50px] rounded-[50px] shadow-md shadow-gray-200 text-gray-100 bg-gray-200 flex items-center justify-center col-span-1'>۳</div>
            <h5 className='text-xs text-gray-200 pt-2'>۱۴۰۲-۱۲-۲۴</h5>
           </div>
           <div>
-           <div className='w-[50px] h-[50px] rounded-[50px] shadow-md shadow-fuchsia-400 text-fuchsia-400 bg-fuchsia-100 flex items-center justify-center col-span-1'>۲</div>
-           <h5 className='text-xs text-transparent pt-2'> .</h5>
-          </div>
+           <div className='w-[50px] h-[50px] rounded-[50px] shadow-md shadow-gray-200 text-gray-100 bg-gray-200 flex items-center justify-center col-span-1'>۲</div>
+           <h5 className='text-xs text-gray-200 pt-2'>۱۴۰۳-۰۳-۲۴</h5>
+           </div>
           <div>
            <div className='w-[50px] h-[50px] rounded-[50px] shadow-md shadow-gray-200 text-gray-100 bg-gray-200 flex items-center justify-center col-span-1'>۱</div>
            <h5 className='text-xs text-gray-200 pt-2'>۱۴۰۲-۰۶-۲۴</h5>
--- src/components/twenty-lottery.jsx
+++ src/components/twenty-lottery.jsx
@@ -33,12 +33,12 @@ class TwentyFourLottery extends Component {
               <h5 className='text-xs text-transparent pt-2'> .</h5>
             </div>
             <div>
-              <div className='w-[50px] h-[50px] rounded-[50px] shadow-md shadow-cyan-400 text-cyan-400 bg-cyan-100 flex items-center justify-center col-span-1'>۹</div>
-              <h5 className='text-xs text-transparent pt-2'> .</h5>
+              <div className='w-[50px] h-[50px] rounded-[50px] shadow-md shadow-gray-200 text-gray-100 bg-gray-200 flex items-center justify-center col-span-1'>۹</div>
+              <h5 className='text-xs text-gray-200 pt-2'>۱۴۰۳-۰۲-۲۰</h5>
             </div>
             <div>
-              <div className='w-[50px] h-[50px] rounded-[50px] shadow-md shadow-md shadow-cyan-400 text-cyan-400 bg-cyan-100 flex items-center justify-center col-span-1'>۸</div>
-              <h5 className='text-xs text-transparent pt-2'> .</h5>
+              <div className='w-[50px] h-[50px] rounded-[50px] shadow-md shadow-md shadow-gray-200 text-gray-100 bg-gray-200 flex items-center justify-center col-span-1'>۸</div>
+              <h5 className='text-xs text-gray-200 pt-2'>۱۴۰۳-۰۳-۲۰</h5>
             </div>
             <div>
               <div className='w-[50px] h-[50px] rounded-[50px] shadow-md shadow-cyan-400 text-cyan-400 bg-cyan-100 flex items-center justify-center col-span-1'>۷</div>
