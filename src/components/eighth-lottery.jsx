e4bd0de166b31ba4fb0fbc39a5b7a32adb057ee8
r.kashir <workkashir2323@gmail.com>
2024-07-29 14:16:38 +0330

    eighth

--- src/components/eighth-lottery.jsx
+++ src/components/eighth-lottery.jsx
@@ -44,8 +44,8 @@ class FifthLottery extends Component {
               <h5 className='text-xs text-gray-200 pt-2'>۱۴۰۳-۰۴-۰۸</h5>
             </div>
             <div className='col-span-3'>
-              <div className='w-[50px] h-[50px] rounded-[50px] shadow-md shadow-amber-400 text-amber-400 bg-amber-200 flex items-center justify-center'>۱۰</div>
-              <h5 className='text-xs text-transparent pt-2'> .</h5>
+              <div className='w-[50px] h-[50px] rounded-[50px] shadow-md shadow-gray-200 text-gray-100 bg-gray-200 flex items-center justify-center'>۱۰</div>
+              <h5 className='text-xs text-gray-200 pt-2 flex justify-center'>۱۴۰۳-۰۵-۰۸</h5>
             </div>
           </div>
         </div>
