7224829ebadcb80377bbbd061ac9d04963bef3a5
Raha <mac@Rahas-MacBook-Pro.local>
2024-10-03 10:52:51 +0330

    updated tenth lottery

--- src/components/eighth-lottery.jsx
+++ src/components/eighth-lottery.jsx
@@ -5,7 +5,7 @@ class FifthLottery extends Component {
     return (
       <>
         <div className='flex-col mt-[40px] md:mt-0 shadow-md p-4'>
-          <div className='mb-10 px-3 py-1 rounded-md shadow-gray-200 text-gray-100 bg-gray-200 font-bold'>صندوق ۸ ام <span className='text-xs'>(شروع از ۰۸-۱۰-۱۴۰۲)</span></div>
+          <div className='mb-10 px-3 py-1 rounded-md shadow-gray-200 text-gray-100 bg-gray-200 font-bold text-center'>صندوق ۸ ام <span className='text-xs'>(شروع از ۰۸-۱۰-۱۴۰۲)</span></div>
           <div className='grid grid-cols-3 gap-x-4 gap-y-5 place-items-center'>
             <div>
               <div className='w-[50px] h-[50px] rounded-[50px] shadow-md shadow-gray-200 text-gray-100 bg-gray-200 flex items-center justify-center col-span-1'>۳</div>
