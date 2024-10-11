509c7f1b9b94cb6271e22b1f568f3bf5ab969534
Raha <mac@Rahas-MacBook-Pro.local>
2024-10-11 18:24:32 +0330

    updated twenty lottery and remove eight lottery

--- src/components/twenty-lottery.jsx
+++ src/components/twenty-lottery.jsx
@@ -9,8 +9,8 @@ class TwentyFourLottery extends Component {
           <div className='mb-10 px-3 py-1 rounded-md bg-cyan-100 text-center text-cyan-400 font-bold'>صندوق ۲۰ ام <span className='text-xs'>(شروع از ۲۰-۱۲-۱۴۰۲)</span></div>
           <div className='grid grid-cols-3 gap-x-4 gap-y-5 place-items-center'>
             <div>
-              <div className='w-[50px] h-[50px] rounded-[50px] shadow-md shadow-cyan-400 text-cyan-400 bg-cyan-100 flex items-center justify-center col-span-1'>۳</div>
-              <h5 className='text-xs text-transparent pt-2'> .</h5>
+              <div className='w-[50px] h-[50px] rounded-[50px] shadow-md shadow-gray-200 text-gray-100 bg-gray-200 flex items-center justify-center col-span-1'>۳</div>
+              <h5 className='text-xs text-gray-200 pt-2'>۱۴۰۳-۰۷-۲۰</h5>
             </div>
             <div>
               <div className='w-[50px] h-[50px] rounded-[50px] shadow-md shadow-gray-200 text-gray-100 bg-gray-200 flex items-center justify-center col-span-1'>۲</div>
