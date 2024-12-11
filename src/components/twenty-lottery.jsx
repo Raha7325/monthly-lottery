f028a0c22c8adc0f9d0c2ecb5f58dc467dc60b20
Raha7325 <f.keshir25@gmail.com>
2024-12-11 13:02:07 +0330

    updated twenty lottery

--- src/components/twenty-lottery.jsx
+++ src/components/twenty-lottery.jsx
@@ -6,7 +6,7 @@ class TwentyFourLottery extends Component {
     return (
       <>
         <div className='flex-col mt-[40px] md:mt-0 shadow-md p-4'>
-          <div className='mb-10 px-3 py-1 rounded-md bg-cyan-100 text-center text-cyan-400 font-bold'>صندوق ۲۰ ام <span className='text-xs'>(شروع از ۲۰-۱۲-۱۴۰۲)</span></div>
+          <div className='mb-10 px-3 py-1 rounded-md bg-gray-200 text-center text-gray-300 font-bold'>صندوق ۲۰ ام <span className='text-xs'>(شروع از ۲۰-۱۲-۱۴۰۲)</span></div>
           <div className='grid grid-cols-3 gap-x-4 gap-y-5 place-items-center'>
             <div>
               <div className='w-[50px] h-[50px] rounded-[50px] shadow-md shadow-gray-200 text-gray-100 bg-gray-200 flex items-center justify-center col-span-1'>۳</div>
@@ -29,15 +29,15 @@ class TwentyFourLottery extends Component {
               <h5 className='text-xs text-gray-200 pt-2'>۱۴۰۳-۰۴-۲۰</h5>
             </div>
             <div>
-              <div className='w-[50px] h-[50px] rounded-[50px] shadow-md shadow-cyan-400 text-cyan-400 bg-cyan-100 flex items-center justify-center col-span-1'>۴</div>
-              <h5 className='text-xs text-transparent pt-2'> .</h5>
+              <div className='w-[50px] h-[50px] rounded-[50px] shadow-md shadow-gray-200 text-gray-100 bg-gray-200 flex items-center justify-center col-span-1'>۴</div>
+              <h5 className='text-xs text-gray-200 pt-2'>۱۴۰۳-۰۹-۲۰</h5>
             </div>
             <div>
               <div className='w-[50px] h-[50px] rounded-[50px] shadow-md shadow-gray-200 text-gray-100 bg-gray-200 flex items-center justify-center col-span-1'>۹</div>
               <h5 className='text-xs text-gray-200 pt-2'>۱۴۰۳-۰۲-۲۰</h5>
             </div>
             <div>
-              <div className='w-[50px] h-[50px] rounded-[50px] shadow-md shadow-md shadow-gray-200 text-gray-100 bg-gray-200 flex items-center justify-center col-span-1'>۸</div>
+              <div className='w-[50px] h-[50px] rounded-[50px] shadow-md shadow-gray-200 text-gray-100 bg-gray-200 flex items-center justify-center col-span-1'>۸</div>
               <h5 className='text-xs text-gray-200 pt-2'>۱۴۰۳-۰۳-۲۰</h5>
             </div>
             <div>
