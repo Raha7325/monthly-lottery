e6590417ba28f66866098cb24d91977ef1930a41
Raha7325 <f.keshir25@gmail.com>
2025-04-07 10:38:33 +0330

    finished tenth lottery

--- src/components/tenth-lottery.jsx
+++ src/components/tenth-lottery.jsx
@@ -4,7 +4,7 @@ class TenthLottery extends Component {
     render() {
         return (
             <div className='flex-col mt-[40px] md:mt-0 shadow-md p-4'>
-                <div className='mb-10 px-3 py-1 rounded-md bg-teal-200 text-center text-teal-400 font-bold'>صندوق ۱۰ ام <span className='text-xs'>(شروع از ۱۰-۰۴-۱۴۰۳)</span></div>
+                <div className='mb-10 px-3 py-1 rounded-md bg-gray-200 text-center text-gray-300 font-bold'>صندوق ۱۰ ام <span className='text-xs'>(شروع از ۱۰-۰۴-۱۴۰۳)</span></div>
                 <div className='grid grid-cols-3 gap-x-4 gap-y-5 place-items-center'>
                     <div>
                         <div className='w-[50px] h-[50px] rounded-[50px] shadow-md shadow-gray-200 text-gray-100 bg-gray-200 flex items-center justify-center col-span-1'>۳</div>
@@ -23,8 +23,8 @@ class TenthLottery extends Component {
                         <h5 className='text-xs text-gray-200 pt-2'>۱۴۰۳-۱۲-۱۰</h5>
                     </div>
                     <div>
-                        <div className='w-[50px] h-[50px] rounded-[50px] shadow-md shadow-teal-400 text-teal-400 bg-teal-200 flex items-center justify-center col-span-1'>۵</div>
-                        <h5 className='text-xs text-transparent pt-2'> .</h5>
+                        <div className='w-[50px] h-[50px] rounded-[50px] shadow-md shadow-gray-200 text-gray-100 bg-gray-200 flex items-center justify-center col-span-1'>۵</div>
+                        <h5 className='text-xs text-gray-200 pt-2'>۱۴۰۴-۰۱-۱۰</h5>
                     </div>
                     <div>
                         <div className='w-[50px] h-[50px] rounded-[50px] shadow-md shadow-gray-200 text-gray-100 bg-gray-200 flex items-center justify-center col-span-1'>۴</div>
