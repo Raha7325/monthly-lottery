58088c0890cff83940546d345c51f95d7a3ec429
r.kashir <workkashir2323@gmail.com>
2025-05-28 13:52:34 +0330

    updated seventh lottery

--- src/components/seventh-lottery.jsx
+++ src/components/seventh-lottery.jsx
@@ -4,7 +4,7 @@ class TenthLottery extends Component {
     render() {
         return (
             <div className='flex-col mt-[40px] md:mt-0 shadow-md p-4'>
-                <div className='mb-10 px-3 py-1 rounded-md bg-orange-200 text-center text-orange-400 font-bold'>صندوق ۷ ام <span className='text-xs'>(شروع از ۰۷-۰۶-۱۴۰۳)</span></div>
+                <div className='mb-10 px-3 py-1 rounded-md bg-gray-200 text-center text-gray-300 font-bold'>صندوق ۷ ام <span className='text-xs'>(شروع از ۰۷-۰۶-۱۴۰۳)</span></div>
                 <div className='grid grid-cols-3 gap-x-4 gap-y-5 place-items-center'>
                     <div>
                         <div className='w-[50px] h-[50px] rounded-[50px] shadow-md shadow-gray-200 text-gray-100 bg-gray-200 flex items-center justify-center col-span-1'>۳</div>
@@ -27,9 +27,9 @@ class TenthLottery extends Component {
                     <h5 className='text-xs text-gray-200 pt-2'>۱۴۰۳-۰۹-۰۷</h5>
                     </div>
                     <div>
-                        <div className='w-[50px] h-[50px] rounded-[50px] shadow-md shadow-orange-400 text-orange-400 bg-orange-200 flex items-center justify-center col-span-1'>۴</div>
-                        <h5 className='text-xs text-transparent pt-2'> .</h5>
-                    </div>
+                        <div className='w-[50px] h-[50px] rounded-[50px] shadow-md shadow-gray-200 text-gray-100 bg-gray-200 flex items-center justify-center col-span-1'>۴</div>
+                        <h5 className='text-xs text-gray-200 pt-2'>۱۴۰۴-۰۳-۰۷</h5>
+                        </div>
                     <div>
                         <div className='w-[50px] h-[50px] rounded-[50px] shadow-md shadow-gray-200 text-gray-100 bg-gray-200 flex items-center justify-center col-span-1'>۹</div>
                         <h5 className='text-xs text-gray-200 pt-2'>۱۴۰۳-۰۷-۰۷</h5>
